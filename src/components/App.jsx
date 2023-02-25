54336f802018e9888744020c99d5817590ca6a4b
Mari <mrs.tereshchenko@gmail.com>
2023-02-25 12:01:17 +0100

    tasks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,84 +1,21 @@
-import { useState, useEffect } from 'react';
-import { nanoid } from 'nanoid';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
-import PhonebookForm from './Phonebook/PhonebookForm';
-import Filter from './Phonebook/Filter';
-import ContactsList from './Phonebook/ContactsList';
+import UserRoutes from './UserRoutes';
 
-const App = () => {
-  const [contacts, setContacts] = useState(() => {
-    const contacts = JSON.parse(localStorage.getItem('contacts'));
-    return contacts ? contacts : [];
-  });
-  const [filter, setFilter] = useState('');
+import { store, persistor } from './redux/store';
 
-  useEffect(() => {
-    localStorage.setItem('contacts', JSON.stringify(contacts));
-  }, [contacts]);
-
-  const addContact = ({ name, number }) => {
-    if (isDublicate(name)) {
-      alert(`${name} is already in contacts list`);
-      return false;
-    }
-
-    setContacts(contacts => {
-      const newContact = {
-        id: nanoid(),
-        name,
-        number,
-      };
-
-      return [newContact, ...contacts];
-    });
-    return true;
-  };
-
-  const isDublicate = name => {
-    const normalizedName = name.toLowerCase();
-    const result = contacts.find(({ name }) => {
-      return name.toLowerCase() === normalizedName;
-    });
-
-    return Boolean(result);
-  };
-
-  const deleteName = id => {
-    setContacts(prevContacts =>
-      prevContacts.filter(contact => contact.id !== id)
-    );
-  };
-
-  const handleFilter = ({ target }) => {
-    setFilter(target.value);
-  };
-
-  const getFilteredContacts = () => {
-    if (!filter) {
-      return contacts;
-    }
-
-    const normalizedFilter = filter.toLowerCase();
-    const result = contacts.filter(({ name }) => {
-      return name.toLowerCase().includes(normalizedFilter);
-    });
-
-    return result;
-  };
-
-  const FilteredContacts = getFilteredContacts();
-  const isName = Boolean(FilteredContacts.length);
-
+function App() {
   return (
-    <>
-      <PhonebookForm onSubmit={addContact} />
-      <Filter handleChange={handleFilter} />
-      {isName && (
-        <ContactsList deleteName={deleteName} contacts={FilteredContacts} />
-      )}
-      {/* {!isName && <p>No contacts in list</p>} */}
-    </>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter basename="/goit-react-hw-06-phonebook">
+          <UserRoutes />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
   );
-};
+}
 
 export default App;
